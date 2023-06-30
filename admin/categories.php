3c3d983f36cacbff58ac2570a3be5b534b8e57ed
Afonso Pereira <afpereira@deloitte.pt>
2023-06-30 15:07:32 +0100

    part2

--- admin/categories.php
+++ admin/categories.php
@@ -26,12 +26,27 @@
                             <div class="form-group">
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
-
-
-
-
-
                         </form>
+                        </div>
+
+                        <div class="col-xs-6">
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Baseball Category</td>
+                                    <td>Basketball Category</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+
                         </div>
 
 
