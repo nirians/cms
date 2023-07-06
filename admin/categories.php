dd7b1e12221a99bf012bea8f3fc81a1498c955c0
Afonso Pereira <afpereira@deloitte.pt>
2023-07-06 09:15:10 +0100

    107. part 2

--- admin/categories.php
+++ admin/categories.php
@@ -53,35 +53,21 @@
                                 </div>
                             </form>
 
-                            <form action="" method="post">
 
-                                <div class="form-group">
-                                    <label for="cat-title">Update Category</label>
+                            <?php 
+                            
+                            if(isset($_GET['edit'])) {
 
-                                    <?php 
+                                $cat_id = $_GET['edit'];
 
-                                    if(isset($_GET['edit'])) {
-                                        $cat_id = $_GET['edit'];
-                                        $query = "SELECT *  FROM categories WHERE cat_id = $cat_id ";
-                                        $select_categories_id = mysqli_query($connection, $query);
+                                include "includes/update_categories.php";
 
-                                        while ($row = mysqli_fetch_assoc($select_categories_id)) {
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
-                                            ?>
+                            }
+                            
+                            
+                            ?>
 
-                                        <input type="text" name="cat_title" class="form-control" value="<?php if (isset($cat_title)) {
-                                            echo $cat_title;
-                                        } ?>"            
-
-                                            <?php } } ?>
-
-                                    <input class="form-control" type="text" name="cat_title">
-                                </div>
-                                <div class="form-group">
-                                    <input class="btn btn-primary" type="submit" name="submit" value="Update">
-                                </div>
-                            </form>
+                            
                         </div>
 
                         <div class="col-xs-6">
