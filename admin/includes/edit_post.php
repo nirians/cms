bb63b87e5e347ccdb7901b50086a2b7ed6099745
Afonso Pereira <afpereira@deloitte.pt>
2023-07-12 12:10:23 +0100

    119. updating posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -4,7 +4,7 @@
         $the_post_id = $_GET['p_id'];
     }
 
-    $query = "SELECT *  FROM posts";
+    $query = "SELECT *  FROM posts WHERE post_id = $the_post_id";
     $select_posts_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
@@ -21,6 +21,37 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 }
 
+if(isset($_POST['update_post'])){
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "UPDATE post SET ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirm($update_post);
+
+}
+
 ?>
 
 
@@ -33,7 +64,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     <div class="form-group">
     
     
-    <select name="" id="">
+    <select name="post_category" id="">
 
         <?php 
         
@@ -85,7 +116,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
         </textarea>
     </div>
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Update Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 
 </form>
