5145933e75a1dbbd3ee5b8c97f7a0efdaa6c44c9
Afonso Pereira <afpereira@deloitte.pt>
2023-07-21 11:17:08 +0100

    chaanges from the db

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -17,7 +17,8 @@
 
 
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
                                 <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
@@ -48,7 +49,8 @@
                             <a href="##"><i class="fa fa-fw fa-file"></i> Comments </a>
                         </li>
                         <li>
-                            <a href="#" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                            <a href="#" data-toggle="collapse" data-target="#demo">
+                                <i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                             <ul id="demo" class="collapse">
                                 <li>
                                     <a href="#">Dropdown Item</a>
--- includes/db.php
+++ includes/db.php
@@ -1,20 +1,13 @@
 <?php 
 
 $db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
+$db['db_name'] = "cmsproject";
+$db['db_user'] = "cmsproject";
+$db['db_pass'] = "BQZjEUyRhoZ4zPEf";
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
 }
 
 
- $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-// if ($connection) {
-// echo "We are connected";
-// }
-
-
-
-?>
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
