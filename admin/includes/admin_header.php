8f445847c710a2c3425c27c13f9216a651d64bc2
Afonso Pereira <afpereira@deloitte.pt>
2023-07-25 17:12:27 +0100

    160. logout validation

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -2,40 +2,14 @@
 <?php include "functions.php"; ?>
 <?php ob_start(); ?>
 <?php session_start(); ?>
-
-
-
-
-
 <?php 
 
 
-
-
-if(isset($_SESSION['user_role'])) {
-
-    if($_SESSION['user_role'] !== 'admin' ) {
-
-        header("Location: ../index.php");
-    
-    }
-
+if(!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
 }
 
-
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -13,7 +13,7 @@
             <!-- Top Menu Items -->
                 <ul class="nav navbar-right top-nav">
                     <li><a href="../index.php">HOME SITE</a></li> 
-
+                    <li><a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
 
 
                     <li class="dropdown">
@@ -24,9 +24,7 @@
                                 <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
                             </li>
                             <li class="divider"></li>
-                            <li>
-                                <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                            </li>
+                            
                         </ul>
                     </li>
                 </ul>
