f55a41a51037ac8279d934db8131268bb2c89f4e
Afonso Pereira <afpereira@deloitte.pt>
2023-07-25 17:42:01 +0100

    section 18

--- admin/profile.php
+++ admin/profile.php
@@ -23,7 +23,33 @@ if (isset($_SESSION['username'])) {
 }
 
 ?>
+<?php 
 
+if (isset($_POST['edit_user'])) {
+
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+
+    $query = "UPDATE users SET ";
+    $query .= "user_firstname = '$user_firstname', ";
+    $query .= "user_lastname = '$user_lastname', ";
+    $query .= "user_role = '$user_role', ";
+    $query .= "username = '$username', ";
+    $query .= "user_email = '$user_email', ";
+    $query .= "user_password = '$user_password' ";
+    $query .= "WHERE username = '{$username}'";
+    $create_user_query = mysqli_query($connection, $query);
+
+     confirm($create_user_query);
+     header("Location: users.php");
+
+}
+
+?>
     <div id="wrapper">
 
     <?php include "includes/admin_navigation.php"; ?>   
