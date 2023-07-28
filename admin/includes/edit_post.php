c15f1c26ca3b09de8209dd5d2c2e9f5c5acbade7
Afonso Pereira <afpereira@deloitte.pt>
2023-07-28 09:38:27 +0100

    summernote

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -139,8 +139,7 @@ if(isset($_POST['update_post'])){
     <div class="form-group">
         <label for="post_content">Post Content</label>
         <textarea class="form-control" name="post_content" id="" cols="30" rows="10" >
-            <?php echo $post_content; ?>
-        </textarea>
+            <?php echo $post_content;?></textarea>
     </div>
     <div class="form-group">
         <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
--- admin/index.php
+++ admin/index.php
@@ -144,6 +144,11 @@ echo "<div class='huge'>{$cat_counts}</div>";?>
 
                 <?php
 
+$query = "SELECT * FROM posts WHERE post_status = 'published'";
+$select_all_published_posts = mysqli_query($connection, $query);
+$post_published_count = mysqli_num_rows($select_all_published_posts);
+
+
 $query = "SELECT * FROM posts WHERE post_status = 'draft'";
 $select_all_draft_posts = mysqli_query($connection, $query);
 $post_draft_count = mysqli_num_rows($select_all_draft_posts);
@@ -175,8 +180,8 @@ $subs_count = mysqli_num_rows($select_all_subs);
 
           <?php
 
-          $element_text = ['Active Post', 'Draft Posts', 'Comments', 'Pending Comments','Users', 'Subscribers' ,'Categories'];
-          $element_count = [$post_counts, $post_draft_count, $comment_counts, $unapproved_count, $user_counts, $subs_count, $cat_counts];
+          $element_text = ['All Posts', 'Active Post', 'Draft Posts', 'Comments', 'Pending Comments','Users', 'Subscribers' ,'Categories'];
+          $element_count = [$post_counts, $post_published_count, $post_draft_count, $comment_counts, $unapproved_count, $user_counts, $subs_count, $cat_counts];
 
           for ($i = 0; $i < 7; $i++) {
               echo "['{$element_text[$i]}'" . " ," . "{$element_count[$i]}],";
