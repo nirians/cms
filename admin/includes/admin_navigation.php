e8f9a741c0fedff1e74b35d615909a470c6939f5
Afonso Pereira <afpereira@deloitte.pt>
2023-09-29 16:25:59 +0100

    changes made

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -15,43 +15,27 @@
         <ul class="nav navbar-right top-nav">
             <li><a href="../index.php">HOME SITE</a></li> 
             <li><a href="../includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
-
-
-            <li class="dropdown">
-                <a href="./profile.php" class="dropdown-toggle" data-toggle="dropdown"> 
-                    <i class="fa fa-user"></i> <?php echo $_SESSION['firstname'];?>
-                    <li class="divider"></li>
-                    
-                </ul>
-            </li>
+            <li><a href="../profile.php"><i class="fa fa-user"></i> <?php echo $_SESSION['firstname'];?></a></li>
         </ul>
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav side-nav">
-                <li>
-                    <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                </li>
-                <li>
-                    <a href="./posts.php">View All Posts</a>
-                </li>
-                <li>
-                    <a href="posts.php?source=add_post">Add Posts</a>
-                </li>
-                <li>
-                    <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
-                </li>
-                <li class="">
-                    <a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments </a>
-                </li>
-                <li>
-                    <a href="users.php">View All Users</a>
-                </li>
-                <li>
-                    <a href="users.php?source=add_user">Add User </a>
-                </li>
-                <li>
-                    <a href="users.php?source=edit_user">Edit User</a>
-                </li>
+                <li><a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a></li>
+                
+                <li><a href="./posts.php">View All Posts</a></li>
+                
+                <li><a href="posts.php?source=add_post">Add Posts</a></li>
+                
+                <li><a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a></li>
+                
+                <li class=""><a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments </a></li>
+                
+                <li><a href="users.php">View All Users</a></li>
+                
+                <li><a href="users.php?source=add_user">Add User </a></li>
+
+                <li><a href="users.php?source=edit_user">Edit User</a></li>
+                
                 <li>
                     <a href="javascript:;" data-toggle="collapse" data-target="#demo">
                         <i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
--- includes/navigation.php
+++ includes/navigation.php
@@ -28,17 +28,17 @@
                     echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                 }               
                 ?>
-                    <li>
-                        <a href="admin">Admin</a>
-                    </li>
-                    <?php 
-                        if(isset($_SESSION['user_role'])) {
-                            if(isset($_GET['p_id'])) {
-                                $the_post_id = $_GET['p_id'];
-                                echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
-                            }
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
+                <?php 
+                    if(isset($_SESSION['user_role'])) {
+                        if(isset($_GET['p_id'])) {
+                            $the_post_id = $_GET['p_id'];
+                            echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
                         }
-                    ?>
+                    }
+                ?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
